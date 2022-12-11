2a7db65ce9aa452dad993fefc3f83c241ba27877
Morteza Toushekhah <mtoshe87@gmail.com>
2022-12-11 13:48:46 +0330

    ecommerce shop react final

---
+++ README.md
@@ -0,0 +1,12 @@
+This e-Commerce shop was created by react framework, using fakestore api (https://fakestoreapi.com/).
+Style of this app was created by CSS framework Tailwind.
+
+Check demo:
+
+Source code:
+
+This app was created by me(Morteza Toushekhah)
+
+my website: https://mortezatoushekhah.netlify.app
+
+Linkedin: https://www.linkedin.com/in/mortezatoshekhah
---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="bg-primary py-12">
+      <div className="container mx-auto">
+        <p className="text-white text-center">
+          Copyright &copy; Ecommerce Shop 2022. All rights reserved. Created by{" "}
+          <a href="https://mortezatoushekhah.netlify.app/">
+            Morteza Toushekhah
+          </a>
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
